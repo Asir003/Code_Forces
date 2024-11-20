a3b54d5fb498ee0c50a470558c1f9d92cf953d5f
Asir003 <161163913+Asir003@users.noreply.github.com>
2024-11-21 00:41:09 +0600

    Add files via upload

---
+++ Again_Twenty_Five.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FLI(i,a,b) for (int i = a; i < b; i++)
+#define FLJ(j,c,d) for (int j = c; j < d; j++)
+ 
+int main()
+{
+    long long n;
+    cin>>n;
+    cout<<"25";
+    return 0;
+    
+}
