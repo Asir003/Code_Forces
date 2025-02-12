b1bd1a552dbb1479b3ec541fc2fd8cf4e3572e88
Asir Hamim <161163913+Asir003@users.noreply.github.com>
2025-02-12 23:52:30 +0600

    Add files via upload

--- Football.cpp
+++ Football.cpp
@@ -2,44 +2,28 @@
 using namespace std;
 #define FLI(i,a,b) for (int i = a; i < b; i++)
 #define FLJ(j,c,d) for (int j = c; j < d; j++)
-int main()
-{
-    string arr;
-    cin>>arr;
-    int number=arr.size();
-    int sum1=0;
-    int sum0=0;
-    int sum1max=0;
-    int sum0max=0;
-    FLI(i,0,number)
-    {
-        if(arr[i]=='1')
-        {
-            sum0=0;
-            sum1++;
-            if(sum1>sum1max)
-            {
-                sum1max=sum1;
-            }
-        }
-        else
-        {
-            sum1=0;
-            sum0++;
-            if(sum0>sum0max)
-            {
-                sum0max=sum0;
-            }
-        }
-    }
-    if(sum1max>=7 || sum0max>=7)
-    {
-        cout<<"YES";
-    }
-    else
-    {
-        cout<<"NO";
 
+int main(){
+    int n;
+    cin>>n;
+    vector<string> team(n);
+    string name;
+    FLI(i,0,n){
+        cin>>team[i];
     }
-    return 0;
+    int sum=0;
+    FLI(i,0,n){
+    int count=0;
+        for(int j=i;j<n;j++){
+            if(team[i]==team[j]){
+                count++;
+            }
+        }
+        //cout<<count<<endl;
+        if(count>=sum){
+            sum=count;
+           name=team[i];
+        }
+    }
+    cout<<name<<endl;
 }
